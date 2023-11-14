b8f68d1514e188cdf539df1c4c58bd779a97380c
aguspranyoto <agusprnyt@gmail.com>
2023-11-14 15:03:01 +0700

    selesai ui detail reastaurant

--- app/detail/[id]/page.jsx
+++ app/detail/[id]/page.jsx
@@ -14,48 +14,148 @@ async function getRestaurant(id) {
 async function DetailPage({ params }) {
   const id = params.id;
   const data = await getRestaurant(id);
-  console.log(data);
   const restaurant = data.restaurant;
   return (
     <>
-      <main className="border-t border-slate-300 mx-auto max-w-5xl py-6 px-4 sm:px-6 lg:px-8 ">
-        <h1 className="text-2xl">Detail Restaurant</h1>
-        <div className="w-full my-5">
-          <h2>{restaurant.name}</h2>
-          <p>{restaurant.description}</p>
-          <p>{restaurant.city}</p>
-          <p>{restaurant.address}</p>
+      <section class="border-t border-slate-300 mx-auto max-w-5xl py-6 px-4 sm:px-6 lg:px-8 ">
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl">Detail Restaurant</h1>
+          <Link href={"/"} className="btn btn-primary">
+            Homepage
+          </Link>
+        </div>
+        <div class="flex flex-col py-5">
           <Image
             src={`https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}`}
             width={300}
             height={300}
             alt="image restaurant"
+            className="mx-auto w-[50%] object-cover object-center rounded border border-gray-200"
           />
-          {restaurant.categories.map((item) => (
-            <div key={item.name}>
-              <p>{item.name}</p>
+          <div class="my-5">
+            <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">
+              {restaurant.name}
+            </h1>
+            <div class="flex mb-4">
+              <span class="flex items-center">
+                <svg
+                  fill="currentColor"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-4 h-4 text-red-500"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                </svg>
+                <svg
+                  fill="currentColor"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-4 h-4 text-red-500"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                </svg>
+                <svg
+                  fill="currentColor"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-4 h-4 text-red-500"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                </svg>
+                <svg
+                  fill="currentColor"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-4 h-4 text-red-500"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                </svg>
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-4 h-4 text-red-500"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                </svg>
+                <span class="text-gray-600 ml-3">{restaurant.rating}</span>
+              </span>
             </div>
-          ))}
-          {restaurant.menus.foods.map((item) => (
-            <div key={item.name}>
-              <p>{item.name}</p>
+            <p class="leading-relaxed">{restaurant.description}</p>
+            <div class="flex mt-6 items-center py-5 border-b-2 border-t-2 border-gray-200 ">
+              <div class="flex">
+                <span class="mr-3">City : {restaurant.city}</span>
+              </div>
+              <div class="flex ml-6 items-center">
+                <span class="mr-3">Address : {restaurant.address}</span>
+              </div>
             </div>
-          ))}
-          {restaurant.menus.drinks.map((item) => (
-            <div key={item.name}>
-              <p>{item.name}</p>
+            <div class="flex items-center py-5 border-b-2 border-gray-200 ">
+              <div>
+                <h2>
+                  Categories :{" "}
+                  {restaurant.categories.map((item, i, row) => (
+                    <span className="ml-2">
+                      {i + 1 === row.length ? `${item.name}.` : `${item.name},`}
+                    </span>
+                  ))}
+                </h2>
+              </div>
             </div>
-          ))}
-          <p>{restaurant.rating}</p>
-          {restaurant.customerReviews.map((item) => (
-            <div key={item.name}>
-              <p>{item.name}</p>
-              <p>{item.review}</p>
-              <p>{item.date}</p>
+            <div class="flex py-5 border-b-2 border-gray-200 flex-col">
+              <h2 className="text-2xl mb-2"> Menus </h2>
+              <div className="flex flex-row w-full">
+                <div>
+                  <h2 className="font-medium">Foods:</h2>
+                  {restaurant.menus.foods.map((item) => (
+                    <div className="flex flex-col">
+                      <span className="">{item.name}</span>
+                    </div>
+                  ))}
+                </div>
+
+                <div className="ml-6">
+                  <h2 className="font-medium">Drinks:</h2>
+                  {restaurant.menus.drinks.map((item) => (
+                    <div className="flex flex-col   ">
+                      <span className="">{item.name}</span>
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
-          ))}
+            <div class="flex py-5 border-b-2 border-gray-200 flex-col">
+              <h2 className="text-2xl mb-2"> Customer Reviews </h2>
+              <div className="flex flex-col w-full">
+                {restaurant.customerReviews.map((item) => (
+                  <div className="card bg-slate-900 text-white w-1/3 mb-2 flex flex-col p-4">
+                    <div className="flex justify-between mb-2 items-center">
+                      <span className="text-xl font-bold">{item.name}</span>
+                      <span className="text-sm">{item.date}</span>
+                    </div>
+                    <span className="">{item.review}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
+      </section>
     </>
   );
 }
